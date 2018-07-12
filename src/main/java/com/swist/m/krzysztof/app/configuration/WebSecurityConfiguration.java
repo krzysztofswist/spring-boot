d574472d8695e14a60e950e0a9113f8e76866ce2
Krzysztof Swist <krzysztof.m.swist@gmail.com>
2018-07-12 14:52:41 +0200

    locale and lang

--- src/main/java/com/swist/m/krzysztof/app/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/swist/m/krzysztof/app/configuration/WebSecurityConfiguration.java
@@ -1,55 +1,80 @@
 package com.swist.m.krzysztof.app.configuration;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
- 
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
- 
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter implements  WebMvcConfigurer{
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        return bCryptPasswordEncoder;
-    }
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
 
- 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
- 
-        http.csrf().disable();
- 
-        // The pages does not require login
-        http.authorizeRequests().antMatchers("/", "/login", "/logout").permitAll();
- 
-        // /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
-        // If no login, it will redirect to /login page.
-        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
- 
-        // For ADMIN only.
-        http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
- 
-        // When the user has logged in as XX.
-        // But access a page that requires role YY,
-        // AccessDeniedException will be thrown.
-        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
- 
-        // Config for Login Form
-        http.authorizeRequests().and().formLogin()//
-                // Submit URL of login page.
-                .loginProcessingUrl("/j_spring_security_check") // Submit URL
-                .loginPage("/login")//
-                .defaultSuccessUrl("/userInfo")//
-                .failureUrl("/login?error=true")//
-                .usernameParameter("username")//
-                .passwordParameter("password")
-                // Config for Logout Page
-                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
- 
-    }
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.getDefault());
+		//slr.setDefaultLocale(new Locale("pl", "POLAND", "WIN"));
+		return slr;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry){
+	    registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable();
+
+		// The pages does not require login
+		http.authorizeRequests().antMatchers("/", "/login", "/logout").permitAll();
+
+		// /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
+		// If no login, it will redirect to /login page.
+		http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+
+		// For ADMIN only.
+		http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
+
+		// When the user has logged in as XX.
+		// But access a page that requires role YY,
+		// AccessDeniedException will be thrown.
+		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+
+		// Config for Login Form
+		http.authorizeRequests().and().formLogin()//
+				// Submit URL of login page.
+				.loginProcessingUrl("/j_spring_security_check") // Submit URL
+				.loginPage("/login")//
+				.defaultSuccessUrl("/userInfo")//
+				.failureUrl("/login?error=true")//
+				.usernameParameter("username")//
+				.passwordParameter("password")
+				// Config for Logout Page
+				.and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
+
+	}
 }
